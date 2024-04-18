bed4769d2812d914c9542936d553eaac6ddaccf8
lionel-arnaud-deltatre <94364913+lionel-arnaud-deltatre@users.noreply.github.com>
2024-04-18 17:43:26 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-echo "actionType: $1"
-echo "appName: $2"
-echo "deviceType: $3"
-echo "version: $4"
-echo "folderPath: $5"
-echo "filePath: $6"
-
 cd /usr/src/app
-ls
-node ./storeFolder/index.js
+node ./src/index.js
