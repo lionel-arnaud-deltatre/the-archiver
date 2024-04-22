7be3ef2b5bd621a860e7b53204e9bc17dc5d7c55
lionel-arnaud-deltatre <94364913+lionel-arnaud-deltatre@users.noreply.github.com>
2024-04-22 14:30:15 +0100

    Update commit.sh

--- workflows/commit.sh
+++ workflows/commit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd $1
-echo "commiting from $var"
+echo "commiting from $1"
+git config --global --add safe.directory $1
 git config --global "user.email" "action@github.com"
 git config --global "user.name" "GitHub Actions"
 git add .
