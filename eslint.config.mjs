4da9c7a05bb0951180b901bb1d338c544a00f287
lionel-arnaud-deltatre <94364913+lionel-arnaud-deltatre@users.noreply.github.com>
2024-04-24 16:15:43 +0100

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,10 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
 import stylisticJs from '@stylistic/eslint-plugin-js';
 import { FlatCompat } from '@eslint/eslintrc';
 
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({ baseDirectory: __dirname });
