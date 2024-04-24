cd1eed73d029497e138fdf98b7ae3a01fead64c3
lionel-arnaud-deltatre <94364913+lionel-arnaud-deltatre@users.noreply.github.com>
2024-04-24 14:17:23 +0100

    Update UpdateRollbackWorkflow.js

--- src/commands/workflows/UpdateRollbackWorkflow.js
+++ src/commands/workflows/UpdateRollbackWorkflow.js
@@ -5,12 +5,21 @@ class UpdateRBWorkflow {
   constructor() {}
 
   async execute(params, versionsArray) {
-    
-    const versionsString = versionsArray.map((item) => `- "${item.version}"`).join("\n          ");
-    console.log("UpdateRBWorkflow", versionsString)
+    const versionsString = versionsArray
+      .map(
+        (item) =>
+          `- "${item.version}" (${this.formatDate(item.lastModified)}) - ${this.bytesToMB(item.size)}`
+      ).join("\n          ");
+    console.log("UpdateRBWorkflow", versionsString);
 
-    const srcFile = path.join(__dirname, "../../../resource/.rollback-template");
-    const destFile = path.join(process.env.GITHUB_WORKSPACE,`.github/workflows/rollback_${params.deviceType}_${params.environment}.yml`);
+    const srcFile = path.join(
+      __dirname,
+      "../../../resource/.rollback-template"
+    );
+    const destFile = path.join(
+      process.env.GITHUB_WORKSPACE,
+      `.github/workflows/rollback_${params.deviceType}_${params.environment}.yml`
+    );
 
     if (!fs.existsSync(srcFile)) {
       console.error("src file does not exists");
@@ -21,13 +30,13 @@ class UpdateRBWorkflow {
       const data = fs.readFileSync(srcFile, "utf8");
 
       const replacements = [
-        ["<device_type>", params.deviceType], 
-        ["<env>", params.environment], 
-        ["<project_name>", params.appName], 
-        ["<versions_placeholder>", versionsString]
-      ]
+        ["<device_type>", params.deviceType],
+        ["<env>", params.environment],
+        ["<project_name>", params.appName],
+        ["<versions_placeholder>", versionsString],
+      ];
 
-      const result = this.replaceAll(data ,replacements);
+      const result = this.replaceAll(data, replacements);
       fs.writeFileSync(destFile, result);
 
       return true;
@@ -40,10 +49,34 @@ class UpdateRBWorkflow {
   replaceAll(str, replacements) {
     for (let index = 0; index < replacements.length; index++) {
       const pair = replacements[index];
-      const regex = new RegExp(pair[0], 'g');
+      const regex = new RegExp(pair[0], "g");
       str = str.replace(regex, pair[1]);
     }
     return str;
+  }
+
+  formatDate(src) {
+    const date = new Date(src);
+
+    // Format date in a readable form, e.g., "April 24, 2024 12:33 PM"
+    return (
+      date.toLocaleDateString("en-US", {
+        month: "long", // "long", "short" or "numeric"
+        day: "2-digit", // "2-digit" or "numeric"
+        year: "numeric", // "2-digit" or "numeric"
+      }) +
+      " " +
+      date.toLocaleTimeString("en-US", {
+        hour: "2-digit", // "2-digit" or "numeric"
+        minute: "2-digit", // "2-digit" or "numeric"
+        hour12: true, // true for AM/PM, false for 24-hour clock
+      })
+    );
+  }
+
+  bytesToMB(bytes) {
+    const MB = bytes / 1024 / 1024;
+    return MB.toFixed(2); // Rounds to two decimal places
   }
 }
 
