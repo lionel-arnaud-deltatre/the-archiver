bf553481e45ac49f9d9defc611639a54a216c87d
lionel-arnaud-deltatre <94364913+lionel-arnaud-deltatre@users.noreply.github.com>
2024-04-26 16:58:00 +0100

    fff

--- resource/zip_file.sh
+++ resource/zip_file.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 echo "=> zip -q $1 $2"
-zip -rq $1 $2
+
+DIST_PATH="${$2%/*}"
+ITEM="${$2##*/}"
+
+cd "$DIST_PATH" || exit 1
+
+zip -rq $1 "$ITEM"
--- resource/zip_folder.sh
+++ resource/zip_folder.sh
@@ -3,5 +3,5 @@
 #zip -r output_file_name.zip /path/to/folder
 echo "=> zip -rq $1 $2"
 
-cd "$2"
+cd "$2" || exit 1
 zip -rq $1 ./*
