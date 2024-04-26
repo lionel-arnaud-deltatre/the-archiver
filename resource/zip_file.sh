b65bfe907d873a37e2efa54846f4a1f258d5bea4
lionel-arnaud-deltatre <94364913+lionel-arnaud-deltatre@users.noreply.github.com>
2024-04-26 17:06:41 +0100

    Update zip_file.sh

--- resource/zip_file.sh
+++ resource/zip_file.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Echo the intended command (useful for debugging)
 echo "=> zip -q $1 $2"
 
 # Extract the directory path and filename/item
 DIST_PATH="${2%/*}"
 ITEM="${2##*/}"
 
+echo "cd to $DIST_PATH"
+echo "and zip $ITEM"
+
 # Change directory
 cd "$DIST_PATH" || { echo "Failed to change directory to $DIST_PATH"; exit 1; }
 
 # Zip the items
-zip -q "$1" "$ITEM"
+zip "$1" "$ITEM"
