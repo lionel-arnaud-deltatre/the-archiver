7fa5dccbc263908d450f05db2baa666596b616a0
lionel-arnaud-deltatre <94364913+lionel-arnaud-deltatre@users.noreply.github.com>
2024-04-26 11:08:14 +0100

    Update zip_folder.sh

--- resource/zip_folder.sh
+++ resource/zip_folder.sh
@@ -2,4 +2,10 @@
 
 #zip -r output_file_name.zip /path/to/folder
 echo "=> zip -rq $1 $2"
-zip -rq $1 $2
+
+DIR_PATH=$(dirname "$2")
+TARGET_DIR=$(basename "$2")
+
+cd "$DIR_PATH"
+
+zip -rq $1 "$TARGET_DIR"
