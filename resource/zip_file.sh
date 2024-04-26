d2d9da21eb99305da000898168f81604cdef303f
lionel-arnaud-deltatre <94364913+lionel-arnaud-deltatre@users.noreply.github.com>
2024-04-26 17:37:53 +0100

    Update zip_file.sh

--- resource/zip_file.sh
+++ resource/zip_file.sh
@@ -3,14 +3,19 @@
 # Echo the intended command (useful for debugging)
 echo "=> zip -q $1 $2"
 
-# Extract the directory path
+# Extract the directory path and filename/item
 DIST_PATH="${2%/*}"
 ITEM="${2##*/}"
 
 echo "cd to $DIST_PATH"
-echo "and zip $ITEM"
+echo "and attempt to zip $ITEM"
 
 # Change directory
 cd "$DIST_PATH" || { echo "Failed to change directory to $DIST_PATH"; exit 1; }
 
-zip -q $1 . --include '$ITEM'
+# List files for debugging
+echo "Listing files in the directory:"
+ls
+
+# Use the wildcard to zip all APK files in the directory using double quotes for variables
+zip -q "$1" . --include "$ITEM"
